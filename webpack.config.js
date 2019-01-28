587f0243f71875ae2ed2b59593050c0d063cef8d
Gopi Gokaraju <gopi.g@COMAKEIT.COM>
2019-01-28 18:43:08 +0530

    files chagned

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebPackPlugin({
-          template: "./src/index.html",
+          template: "./public/index.html",
           filename: "./index.html"
         }),
         new MiniCssExtractPlugin({
