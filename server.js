b511d76df097d5ed6c0119ad967f59b7d953c925
gopi krishnam raju <purushotham.0019@gmail.com>
2019-01-28 20:58:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 
 var PORT = process.env.PORT || 9000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, function() {
