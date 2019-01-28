42155d588ddc54190c3a24c1bedf75d58f384acf
gopi krishnam raju <purushotham.0019@gmail.com>
2019-01-28 20:53:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const path = require('path');
 var app = express();
 
 
-var PORT = process.env.PORT || 5000;
-app.use(express.static(__dirname + '/public'));
+var PORT = process.env.PORT || 9000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, function() {
 		console.log("listening to port" + PORT);
 });
